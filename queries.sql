80b87658f8019f16108577d4cf8b49a8d762a39d
ST-PV <79085486+ST-PV@users.noreply.github.com>
2023-06-22 01:30:56 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,15 +1,31 @@
 SELECT COUNT(customers.customer_id) AS customers_count 
 FROM customers;
 
+
+-- Шаг 5, таблица 2 --- Начало
+SELECT 
+	(employees.first_name ||' '|| employees.last_name) AS name, 
+	ROUND(AVG(sales.quantity * products.price), 0) AS average_income 
+FROM sales
+JOIN employees ON employees.employee_id = sales.sales_person_id
+JOIN products ON products.product_id = sales.product_id
+GROUP BY(employees.first_name ||' '|| employees.last_name)
+HAVING 										-- задал отбор по сгруппированным значениям
+	AVG(sales.quantity * products.price) < 					-- при помощи вложенной функции отобрал нужные значения
+	(SELECT AVG(sales.quantity * products.price) FROM sales JOIN products ON products.product_id = sales.product_id)
+ORDER BY average_income ASC;
+
+-- Шаг 5, таблица 2 --- Конец
+
 -- Шаг 5, таблица 3 --- Начало
-select 
-	(employees.first_name ||' '|| employees.last_name) as name, 
-	to_char(sales.sale_date, 'Day') AS weekday, -- представление дня недели в словесном формате
+SELECT 
+	(employees.first_name ||' '|| employees.last_name) AS name, 
+	to_char(sales.sale_date, 'Day') AS weekday, 			-- представление дня недели в словесном формате
 	ROUND(SUM(sales.quantity * products.price), 0) AS income
 FROM sales
 JOIN employees ON employees.employee_id = sales.sales_person_id
 JOIN products ON products.product_id = sales.product_id
-group by weekday, date_part ('isodow', sales.sale_date), (employees.first_name ||' '|| employees.last_name) -- из интересного тут только группировка по порядковому номеру дня недели
-order by date_part ('isodow', sales.sale_date);
+GROUP BY weekday, date_part ('isodow', sales.sale_date), (employees.first_name ||' '|| employees.last_name) -- из интересного тут только группировка по порядковому номеру дня недели
+ORDER BY date_part ('isodow', sales.sale_date);
 
 -- Шаг 5, таблица 3 --- Конец
